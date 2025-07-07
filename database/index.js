eef2115078e9ede0545a76f2daf8a861df39149d
behwa <billy_behwa@hotmail.com>
2025-07-07 23:55:12 +0800

    migrate login function

--- database/index.js
+++ database/index.js
@@ -1,15 +1,27 @@
 // database/index.js
 const { Pool } = require('pg');
 
+// if(process.env) {
+//   console.log('process.env = ' + process.env)
+//   console.log('NODE_ENV = ' + process.env.NODE_ENV);
+//   if(process.env.NODE_ENV) {
+//     console.log('rocess.env.NODE_ENV = ' + process.env.NODE_ENV)
+//   }
+// }
+
+
 const pool = new Pool({
   user: process.env.PG_USER || 'postgres',
   host: process.env.PG_HOST || 'localhost',
   database: process.env.PG_DATABASE || 'myfreshapp',
   password: process.env.PG_PASSWORD || '1234',
   port: process.env.PG_PORT || 5432,
-    // ssl: false,  // Disable SSL
-  // ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
-    ssl: {
+
+  // Below for Development Local PC Database
+  // ssl: false,  // Disable SSL
+
+  // Below for Production when commit
+  ssl: {
     rejectUnauthorized: false, // required for DO SSL
   }
 });
